fe24a75536d292221d8c47908c856ed7e7782430
JeranDev <jeranjb@gmail.com>
2021-10-29 21:33:46 -0600

    add second api

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const request = require('postman-request')
 require('dotenv').config()
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=37.8267,-122.4233&units=f`
+const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_API_KEY}&query=37.8267,-122.4233&units=f`
+const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_API_KEY}`
 
 request({ url: url, json: true }, (error, response) => {
   const weather = response.body.current
   console.log(
     `${weather.weather_descriptions[0]}. It is currently ${weather.temperature} degrees out. It feels like ${weather.feelslike} degrees out.`
   )
+})
+
+request({ url: geoUrl, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[1]
+  const longitude = response.body.features[0].center[0]
+  console.log(latitude, longitude)
 })
